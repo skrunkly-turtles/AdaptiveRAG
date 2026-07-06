c0430deca6859745758f92711e2d1b61dd64be11
Katelyn Wong <katelynwong100@gmail.com>
2026-07-06 10:55:43 -0400

    making architecture

--- app/new_rag/captain.py
+++ app/new_rag/captain.py
@@ -1,6 +1,100 @@
 """
 This file represents the Captain, which oversees all the firefighters. It is responsible for:
 (1) Maintaining a memory and a cache of conversation and top-level summaries of the firefighters
-(2) Reading the query and parsing meaning 
-(3) Sending the query to the correct firefighters, and determining TIME.
+(2) Reading the query and delivering response
+(3) Sending the query to the correct firefighters, determining TIME, and urgency.
 """
+
+import asyncio
+from models import Time, Query, CapDecision, CapMemory, Answer
+import generator
+import ollama
+import random
+
+client = ollama.AsyncClient()
+memory = CapMemory(
+    firefighter_summary={
+        "ff1": "Status: None",
+        "ff2": "Status: None"
+    }
+)
+
+# The maximum number of raw messages to keep in the context prompt
+MAX_TURNS = 6
+
+# This is the system prompt
+SYS_PROMPT = (""" You are a concise agent. 
+              
+              """)
+
+SUMMARY_PROMPT = ("""sum
+                  
+                  """)
+
+# Compress the conversation_history and updates the summary accordingly
+async def compress_window(self) -> None:
+    """
+    Update data_summary from CapMemory when more than MAX_TURNS of conversation exists. Takes the oldest
+    turns and merges them with the existing data_summary and updates data_summary, then updates data_cache to 
+    the most recent MAX_TURNS of conversation.
+    """
+    # Logging what is happening
+    print("Compressing turns!")
+    try: 
+        response = await client.generate(
+        model='llama3.2:3b',
+        system= SUMMARY_PROMPT,
+        prompt= f"""
+            Context: {data} \n
+            Question: {query.query}
+        """,
+        logprobs= True
+    )
+    except Exception as e:
+        print("Compression failed uh oh :(")
+    raise NotImplementedError
+
+# Answers the query given all the data
+async def answer(q: str) -> str:
+    """
+    Return the answer yay!
+    """
+    print("Pouring water down the drain...")
+    await client.generate(
+        model='llama3.2:3b',
+        system=SYS_PROMPT,
+        prompt= f"""
+            Context: {data} \n
+            Question: {query.query}
+        """,
+        logprobs= True
+    )
+    raise NotImplementedError
+
+async def response_report(q: str) -> str:
+    """
+    Just runs answer() and compress_window() together
+    """
+    await asyncio.gather(
+        answer(q),
+        compress_window()
+    )
+
+# This makes a loop to retrieve and run a response
+async def run_response():
+    while True:
+        r = await response_report(input("Ask a question: \n"))
+        print(f"I have my answer! \n {r} \n")
+        
+        await asyncio.sleep(5) # Wait every five seconds
+
+# Wraps the two asyncio 
+async def main():
+    await asyncio.gather(
+        generator.record_runs(), # This makes the generator make data every two seconds.
+        run_response(),
+    )
+
+if __name__ == '__main__':
+    print("Hi, my name is Jesslyn. I'm listening!")
+    asyncio.run(main())
--- app/new_rag/models.py
+++ app/new_rag/models.py
@@ -4,22 +4,32 @@ This is where all the pydantic models live!
 
 from pydantic import BaseModel
 from datetime import datetime
+from typing import Any
 
-class Time(BaseModel):
+
+# Debating on whether to use this class or not. It uses more tokens than I might need
+class Answer(BaseModel): 
     """
-    The TIME token that reprsents when the data should be pulled from, and how large the window should be
+    The formatted answer from the Captain to the user
     """
-    time: str
-    window: str
+    time: datetime
+    answer: str # The answer yay
+    confidence: float
+
+class ReportList(BaseModel):
+    """
+    The 
+    """
+
 
 class Report(BaseModel):
     """
     The report that is sent from the firefighter to get_data
     """
     time: datetime
-    data: list
-    chunks: int
-    resolution: int
+    data: list # The list of the names of the CSV files we need
+    chunks: int # How many chunks we need
+    resolution: int # How large of a gap we need in between the chunks (i.e. resolution = 2 means every other point for <chunks> times)
 
 class Query(BaseModel):
     """
@@ -27,7 +37,7 @@ class Query(BaseModel):
     """
     query: str
     urgency: float 
-    time: Time
+    time: datetime
 
 class Data(BaseModel):
     """
@@ -67,12 +77,23 @@ class Temp(BaseModel):
     time: datetime
     temp: float
 
-class Summaries(BaseModel):
+class CapDecision(BaseModel):
     """
-    The summaries packet
+    The decision schema used by the captain containing all its decisions 
     """
     time: datetime
-    hr: list
-    o2: list
-    el: list
-    temp: list
+    firefighters: list # The list of firefighters needed
+    data: list[Report] # A list of length firefighters, with each item is the Report corresponding to that ff.
+    urgency: float
+
+class CapMemory(BaseModel):
+    """
+    The context window containg the information that is continually updated by the incoming data, which can be
+    accessed by the Captain to respond to user queries, and send Querys to the Firefighters
+    """
+    last_updated: datetime
+    conversation: list[dict[str, str]] = [] # The memory of the most recent conversation so far with MAX_TURNS
+    data_summary: str = "No prior history yet. See cache." # The summary of what has happened so far!
+    data_cache: dict[str, list[Any]] = {} # Data summary!
+    firefighter_summary: dict[str, str] = {} # A light summary of what each firefighter is doing/looking like :D
+
