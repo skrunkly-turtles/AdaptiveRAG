f88f6b42525abf5a59a02a9ed3813a17431a0739
Katelyn Wong <katelynwong100@gmail.com>
2026-08-06 11:25:21 -0400

    testing

--- app/new_rag/eval.py
+++ app/new_rag/eval.py
@@ -7,14 +7,52 @@ This is the file that is going to evaluate the accuracy of the model. It will do
 """
 import asyncio
 import random
+from pool_maker import process_incoming
+from firefighters import ff1, ff2, ff3
 
 NUM_TESTS = 100
 
-async def data() -> list:
+# The previous value, for data()
+PREV = (0, 0)
+
+# How long the value was the same at that tuple for, for data()
+TIME = 0
+
+async def data() -> tuple:
     """
-    Random generate a series of data streams where 1 = normal, 2 = internal anomaly, 3 = external anomaly
+    Random generates a tuple of information about a single timeframe of data that was generated.
+    The first value is the type of data that was generated; 1 = normal, 2 = internal anomaly, 3 = external anomaly.
+    The second value is the subtype of data that was generated, specified to the type of data as stated above.
+    The third is a dictionary of the data that was generated for that value. 
     """
-    raise NotImplementedError
+    global PREV
+    global TIME
+
+    i = random.choices([1, 2, 3], weights=[20, 5, 2], k=1)[0]
+
+    if i == 2:
+        j = random.choices([1, 2, 3, 4, 5, 6, 7])
+        if (i, j) == PREV:
+            TIME += 1
+        else:
+            TIME = 0
+        k = internal_bad(j, TIME)
+        PREV = (i, j)
+        return (i, j, k)
+
+    if i == 3:
+        j = random.choices([1, 2, 3, 4])
+        if(i, j) == PREV:
+            TIME += 1
+        else:
+            TIME = 0
+        k = external_bad(j, TIME)
+        PREV = (i, j)
+        return (i, j, k)
+
+    PREV = (i, j)
+    k = normal()
+    return (i, 0, k)
 
 
 # The Three Types of Streams yay
@@ -64,53 +102,105 @@ async def internal_bad(type: int, time: int) -> dict:
         if type == 2:
             norm["body_temp"] = round(random.uniform(37, 38), 2)
         return norm
+
     if type == 3:
         norm["body_temp"] = round(random.uniform(38, 40), 2)
         norm["hr"] = int(random.uniform(60, 160))
+
     if type == 4:
         norm["gait"] = round(random.uniform(3, 7), 2)
         norm["respiration"] = int(random.uniform(20, 30))
+
     if type == 5:
         norm["o2"] = round(random.uniform(85, 93), 2)
         norm["hr"] = int(random.uniform(80, 150))
         norm["respiration"] = int(random.uniform(20, 30))
+
     if type == 6:
         norm["respiration"] = 0
         norm["o2"] = round(random.uniform(85, 93), 2)
-    
-    raise NotImplementedError
 
-async def external_bad(type: int) -> dict:
+    if type == 7:
+        norm["respiration"] = int(random.uniform(26, 40))
+        norm["hr"] = int(random.uniform(100, 150))
+
+    return norm
+
+
+async def external_bad(type: int, time: int) -> dict:
     """
     Returns a dict of external anomaly vital signs where the type will be classified as such:
     1 = high elevation
-    2 = low elevatino
+    2 = low elevation
     3 = fire or very high heat
     4 = cold or very low temperature
     """
+    norm = normal()
+    if type == 1:
+        if time > 1:
+            norm["elevation"] *= round(random.uniform(1, 1.1), 2)
+            norm["respiration"] *= round(random.uniform(1, 1.1), 2)
+            return norm
+        norm["elevation"] = round(random.uniform(15, 25), 2)
+
+    if type == 2:
+        if time > 1:
+            norm["elevation"] *= round(random.uniform(0.9, 1), 2)
+            return norm
+        norm["elevation"] = round(random.uniform(-13, 0)) 
+
+    if type == 3:
+        if time < 3:
+            norm["temp"] = round(random.uniform(40, 100), 2)
+        if time > 2:
+            norm["temp"] *= round(random.uniform(1, 1.1))
+        if time > 5:
+            norm["hr"] = int(random.uniform(100, 200))
+            norm["respiration"] = int(random.uniform(15, 25))
+
+    if type == 4:
+        if time < 3:
+            norm["temp"] = round(random.uniform(-25, -10), 2)
+        if time > 2:
+            norm["temp"] *= round(random.uniform(1, 1.05), 2)
+
+    return norm
+
+async def record_matrix(p1: list, p2: list) -> dict:
+    """
+    Return a confusion matrix of what a specific firefighter got right where p1 is the list of the type of 
+    data that was generated, and p2 is the type of data that the LLM flagged. 
+    """
     raise NotImplementedError
 
 
-# This is where the con
+async def run_stream(p1, p2, p3) -> dict:
+    """
+    Return a dictionary of what each LLM decided the state of the place environment was
+    """
+    f1 = []
+    f2 = []
+    f3 = []
 
-async def write_data() -> None:
-    """
-    This is where it decides the type of data and also 
-    """
-async def record_matrix(p1: list, p2: list, p3: list) -> dict:
-    """
-    Return a dictionary of the confusion matrix that the LLM got right.
-    """
-    raise NotImplementedError
+    # The counter for seconds
+    i = 0
+    while i < NUM_TESTS:
+        d1 = data()
+        d2 = data()
+        d3 = data()
+        f1.append(d1[0])
+        f2.append(d2[0])
+        f3.append(d3[0])
 
-async def start_stream() -> None:
-    """
-    This is the main stream that is going to be sending the data to the 
-    """
-    p1 = data()
-    p2 = data()
-    p3 = data()
-    record_matrix(p1, p2, p3)
+        await process_incoming(d1[2], 1)
+        await process_incoming(d2[2], 2)
+        await process_incoming(d3[2], 3)
+
+        i += 1
+        asyncio.sleep(2)
+
+    record_matrix()
+    
 
 if __name__ == "__main__":
-    asyncio.run(start_stream())
+    asyncio.run(run_stream())
